64d21219e9e88809e66a9d5444573c2789d80c5b
walidmostafa10 <111182017+walidmostafa10@users.noreply.github.com>
2024-09-10 00:01:38 +0300

    editdark

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -6,23 +6,23 @@ import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 const DarkMode = () => {
   const [darkMode, setDarkMode] = useState(() => {
-    const savedMode = localStorage.getItem("PortfolioDarkMode");
+    const savedMode = localStorage.getItem("walidPortfolioDarkMode");
     return savedMode !== null ? JSON.parse(savedMode) : true;
   });
 
   const handleDark = () => setDarkMode((prev) => !prev);
 
   useEffect(() => {
-    localStorage.setItem("PortfolioDarkMode", JSON.stringify(darkMode));
+    localStorage.setItem("walidPortfolioDarkMode", JSON.stringify(darkMode));
 
-    darkMode
+    !darkMode
       ? document.body.classList.add("light")
       : document.body.classList.remove("light");
   }, [darkMode]);
 
   return (
     <div className="Nav-icon" onClick={handleDark}>
-      <FontAwesomeIcon icon={darkMode ? faMoon : faSun} />
+      <FontAwesomeIcon icon={!darkMode ? faMoon : faSun} />
     </div>
   );
 };
